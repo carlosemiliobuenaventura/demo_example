ded7dcc68df1bd7cf1b3d8efd02bce3006fb7403
carlosemiliobuenaventura <carlosemiliobuenaventura>
2025-05-26 13:16:01 +0000

    change extension

---
+++ src/ide_integration/demo_isp.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+from databricks.connect import DatabricksSession
+
+spark = DatabricksSession.builder.profile("isp_new_env_demo").getOrCreate()
+
+df = spark.read.table("samples.nyctaxi.trips")
+df.show(5)
+
+# COMMAND ----------
+
+df = spark.read.option('asOfVersion', 4) \
+    .table("isp_demo_carlos.session_2.employee")
+df.show(5)
